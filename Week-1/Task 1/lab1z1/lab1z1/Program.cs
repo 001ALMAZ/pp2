e0e8ac0b91f0f219a43174c7d0c59fdf7e2b9128
001ALMAZ <001_almaz@mail.ru>
2019-02-14 21:17:44 +0600

    salution

---
+++ Week-1/Task 1/lab1z1/lab1z1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1z1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine ());
+            List<int> ans = new List<int>();
+            string[] arr = Console.ReadLine().Split();
+            for( int i = 0; i < N; i++)
+            {
+                int a = int.Parse(arr[i]);
+                int ok = 1;
+                for (int j = 2; j <= (int)Math.Sqrt(a) && ok == 1; j++)
+                    if (a % j == 0)
+                        ok = 0;
+                if (a != 1 && ok == 1)
+                    ans.Add(a);
+            }
+            Console.WriteLine(ans.Count());
+            for (int i = 0; i < ans.Count(); i++)
+                Console.Write(ans[i] + " ");
+            Console.ReadKey();
+        }
+    }
+}
